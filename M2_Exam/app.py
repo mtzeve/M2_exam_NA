e1462056528b2f3cd5ed81b2123b52b3043a8bd9
Manos Tzeve <114588453+mtzeve@users.noreply.github.com>
2023-11-05 17:31:00 +0100

    Add files via upload

---
+++ M2_Exam/app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import streamlit.components.v1 as components
+import networkx as nx
+import matplotlib.pyplot as plt
+from pyvis.network import Network
+import got 
+import community.community_louvain as community_louvain
+
+# page config
+
+st.set_page_config(page_title='Streamlit - Network analysis app')
+
+st.title('M2 Network Analysis')
+st.header("Twitter network of congressmen interactions")
+#st.sidebar.header("Filters 📊")
+
+# Introduction
+
+st.markdown("""
+            Twitter congress network analysis on US Congress
+""")
+"""
+
+"""
+
+HtmlFile = open("nx.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read() 
+components.html(source_code, height = 700,width=700)
